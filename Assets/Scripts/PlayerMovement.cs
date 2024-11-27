51ca19e4eb102d1285de563ee9360f5a5d12e90b
SleepyJack01 <102834546+SleepyJack01@users.noreply.github.com>
2024-11-27 19:10:00 +0000

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Scripting.APIUpdating;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Rigidbody rb;
+    
+    [Header("Movement")]
+    [SerializeField] private float moveForce = 10f;
+    private Vector2 moveDirection;
+
+    private void FixedUpdate()
+    {
+        // apply force to the rigidbody
+        rb.AddForce(new Vector3(moveDirection.x, 0, moveDirection.y) * moveForce);
+    }
+
+    public void MovePlayer(InputAction.CallbackContext context)
+    {
+        moveDirection = context.ReadValue<Vector2>();
+    }
+}
